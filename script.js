f3c72bc49d50a14a6a83c07a8e6388efa8056c3e
Jennifer <jenniferpguzman0@gmail.com>
2024-11-24 14:51:08 -0500

    js comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//budgettracker class
 class BudgetTracker{
     constructor(){
         this.totalIncome = 0;
         this.totalExpenses = 0;
     }
+    //method to add income
     addIncome(description,amount) {
         if (amount > 0){
             this.totalIncome += amount;
@@ -14,6 +16,7 @@ class BudgetTracker{
             incomeList.appendChild(incomeItem);
         }
     }
+    //method to add expenses
     addExpenses(description, amount){
         if (amount > 0){
             this.totalExpenses += amount;
@@ -25,12 +28,14 @@ class BudgetTracker{
             expensesList.appendChild(expensesItem);
         }
     }
+    //method to add up summary with balance
     updateSummary(){
         const balance = this.totalIncome - this.totalExpenses;
         document.getElementById("total-income").textContent = this.totalIncome;
         document.getElementById("total-expenses").textContent = this.totalExpenses;
         document.getElementById("balance").textContent = balance;
     }
+    //method clearing values
     clearAll(){
         this.totalIncome = 0;
         this.totalExpenses = 0;
@@ -40,13 +45,13 @@ class BudgetTracker{
         document.getElementById("expenses-list").innerHTML = "";
     }
 }
-
+//initializing an instance of budget tracker
 const budgetTracker = new BudgetTracker();
-
+//event listener adding income
 document.getElementById("add-income-btn").addEventListener("click", () =>{
     const incomeDescription = document.getElementById("income-description").value;
     const incomeAmount = parseFloat(document.getElementById("income-amount").value);
-
+//income method being called
     if(incomeDescription && !isNaN(incomeAmount)){
         budgetTracker.addIncome(incomeDescription, incomeAmount);
 
@@ -54,11 +59,11 @@ document.getElementById("add-income-btn").addEventListener("click", () =>{
         document.getElementById("income-amount").value = "";
     }
 });
-
+//event listener adding expenses
 document.getElementById("add-expenses-btn").addEventListener("click", () =>{
     const expensesDescription = document.getElementById("expenses-description").value;
     const expensesAmount = parseFloat(document.getElementById("expenses-amount").value);
-
+//expenses method being called
     if (expensesDescription && !isNaN(expensesAmount)){
         budgetTracker.addExpenses(expensesDescription, expensesAmount);
 
@@ -66,7 +71,7 @@ document.getElementById("add-expenses-btn").addEventListener("click", () =>{
         document.getElementById("expenses-amount").value = "";
     }
 });
-
+//event listener clearing values
 document.getElementById("clear-button").addEventListener("click", () =>{
     budgetTracker.clearAll();
 });
